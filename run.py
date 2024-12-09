a0793298e7c36a70fde42a48d8148b089e7083e6
gcc-bug <by.gdc@outlook.com>
2024-12-09 13:39:41 +0800

    update

--- run.py
+++ run.py
@@ -3,38 +3,48 @@ from qft import *
 from qiskit import qasm2
 import pandas as pd
 from Enola.route import *
-for n in range(5,101):
+import time
+data_list = []
+for n in range(5,51):
+    data = {"num": n}
     cir = QFT(n)
     
     w = -1
     print(f"qft_{n} with c={w}")
+    time_s = time.time()
     maps = cir.LNN_maps(w)
     route = QuantumRouter(cir.num_qubits,maps,cir.gate_list, [cir.num_qubits+2,3],"others")
     route.run()
     route.save_program(f"{'linear_map' if w < 0 else 'fold_map'}/qft_{n}_full_code.json")
+    data["LNN time"] = time.time() - time_s
     
     w = 10
     print(f"qft_{n} with c={w}")
+    time_s = time.time()
     maps = cir.LNN_maps(w)
     route = QuantumRouter(cir.num_qubits,maps,cir.gate_list, [12,12],"others")
     route.run()
     route.save_program(f"{'linear_map' if w < 0 else 'fold_map'}/qft_{n}_full_code.json")
+    data["fold time"] = time.time() - time_s
+    data_list.append(data)
 
+# for n in range(200,1001,100):
+#     cir = QFT(n)
+    
+#     w = -1
+#     print(f"qft_{n} with c={w}")
+#     maps = cir.LNN_maps(w)
+#     route = QuantumRouter(cir.num_qubits,maps,cir.gate_list, [cir.num_qubits+2,3],"others")
+#     route.run()
+#     route.save_program(f"{'linear_map' if w < 0 else 'fold_map'}/qft_{n}_full_code.json")
+    
+#     w = math.ceil(math.sqrt(n))
+#     d = math.ceil(n//(w-2))
+#     print(f"qft_{n} with c={w}")
+#     maps = cir.LNN_maps(w)
+#     route = QuantumRouter(cir.num_qubits,maps,cir.gate_list, [w+2,d+2],"others")
+#     route.run()
+#     route.save_program(f"{'linear_map' if w < 0 else 'fold_map'}/qft_{n}_full_code.json")
 
-for n in range(200,1001,100):
-    cir = QFT(n)
-    
-    w = -1
-    print(f"qft_{n} with c={w}")
-    maps = cir.LNN_maps(w)
-    route = QuantumRouter(cir.num_qubits,maps,cir.gate_list, [cir.num_qubits+2,3],"others")
-    route.run()
-    route.save_program(f"{'linear_map' if w < 0 else 'fold_map'}/qft_{n}_full_code.json")
-    
-    w = math.ceil(math.sqrt(n))
-    d = math.ceil(n//(w-2))
-    print(f"qft_{n} with c={w}")
-    maps = cir.LNN_maps(w)
-    route = QuantumRouter(cir.num_qubits,maps,cir.gate_list, [w+2,d+2],"others")
-    route.run()
-    route.save_program(f"{'linear_map' if w < 0 else 'fold_map'}/qft_{n}_full_code.json")
+data_df = pd.DataFrame(data_list)
+data_df.to_csv("time.csv",index=False)
